e831331efb3fb8ab915f5e9ea99db04da0b97df2
Ian Lotto <ianlotto@gmail.com>
2013-12-02 18:44:57 -0500

    Lays out methods

--- wordlist.rb
+++ wordlist.rb
@@ -1,3 +1,27 @@
 class Wordlist
+  attr_reader :list
   
+  def initialize(list)
+    
+  end
+  
+  def by_word_length(max_length = nil)
+    
+  end
+  
+  def subwords_from_length
+    
+  end
+  
+  private
+  
+    def filter_words
+    
+    end
+    
+    def find_pairs
+      
+    end
+    
+    
 end
